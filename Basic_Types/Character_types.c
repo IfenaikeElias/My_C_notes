168fe05b4293d21cff3e94d2aa20896502255fc6
IfenaikeElias <elias.ifenaike@gmail.com>
2025-10-01 12:10:42 +0100

    Learned more on types and type casting in c

--- Basic_Types/Character_types.c
+++ Basic_Types/Character_types.c
@@ -25,4 +25,40 @@ we could even use it in for loops:
 for (ch = "A"; ch = "Z", ch ++)...
 */
 
+/*
+when taking in standard input with scanf. if you wish that white space are not taking 
+with the character yuo just add space like as in " %c" instead of just "%c". 
 
+*/
+
+// Read and writing characters with getchar and putchar
+/*
+getchar() can be used in place of scanf(), it is more faster but it takes in a character and stores it into a variable
+*/
+
+// count how long is a sentence
+
+// int main(void){
+//     char ch;
+//     int len = 0;
+//     printf("Enter a Message: ");
+//     ch = getchar();
+//     while ( ch != '\n') {
+//         len++;
+//         ch = getchar();
+//     };
+//     printf("Your message is %d characters long", len);
+//     return 0;
+// }
+
+
+// a more idiomatic way to write this would be
+
+int main(void){
+    int len = 0;
+    printf("Enter a message: ");
+    while (getchar() != '\n')
+        len ++;
+    printf("Your message was %d characters long.\n", len);
+    return 0;    
+}
